1e130376534705e0ad9c486e68d84d50b039a376
vivek <vivek@athenaeum.in>
2021-09-27 13:38:57 +0530

    done

--- src/store/users-sice.js
+++ src/store/users-sice.js
@@ -1,28 +1,31 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { restClient } from '../services/rest-service';
 
-//  asynchronous action creators are called thunks
-//  they are named: slice-name/thunk-name
-//  A function that accepts a Redux action type string
-//  and a callback function that should return a promise.
-//  It generates promise lifecycle action types based
-//  on the action type prefix that you pass in, and returns
-//  a thunk action creator that will run the promise callback
-//  and dispatch the lifecycle actions based on the returned promise.
 export const getUserById = createAsyncThunk(
   'user/getUserById',
   async (id, thunk) => {
-    //    async callbacks can take only one argument
-    //    consider using objects in case you have to pass multiple arguments
-    const httpResponse = await restClient.get(`users/${id}`);
-    return httpResponse.data;
+    //    thunk object provides the api necessary for decision support
+    //    we can get the current state, chain the promises using dispatch, etc
+    //    get the currentUser by destructuring
+    const {
+      user: { currentUser },
+    } = thunk.getState();
+
+    //    initiate the HTTP call only if the current user is falsy (null/undefined)
+    if (!currentUser) {
+      const httpResponse = await restClient.get(`users/${id}`);
+      return httpResponse.data;
+    }
+
+    //    otherwise, there is no need to make a call
+    return Promise.resolve({
+      notRequired: true,
+    });
   }
 );
 
 export const usersSlice = createSlice({
   name: 'user',
-  //  if the initial state is an elaborate object,
-  //  then the mutation- approach of immer.js can be employed
   initialState: {
     currentUser: null,
     status: null,
@@ -30,15 +33,13 @@ export const usersSlice = createSlice({
   reducers: {
     //... regular reducers here
   },
-  //    thunk reducers go here
   extraReducers: {
-    //    computed property names
-    //    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer#computed_property_names
-    //    three possibilities: pending, fulfilled, and rejected
     [getUserById.fulfilled]: (state, action) => {
-      //    mutation-approach of immer.js
-      state.currentUser = action.payload.data;
-      state.status = 'succeeded';
+      //    if notRequired is true, there is nothing for this reducer to do
+      if (!action.payload.notRequired) {
+        state.currentUser = action.payload.data; // from axios
+        state.status = 'succeeded';
+      }
     },
     [getUserById.rejected]: (state, action) => {
       state.status = 'errored';
