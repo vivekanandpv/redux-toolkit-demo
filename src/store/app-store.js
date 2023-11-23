e9296fb642db9e8ce0f583e28ae02691acb81f8c
Vivekanand P V <vivekanandpv@gmail.com>
2023-11-23 12:51:36 +0530

    done

--- src/store/app-store.js
+++ src/store/app-store.js
@@ -1,12 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { languageSlice } from './language-slice';
-import { loginSlice } from './login-slice';
-import { themeSlice } from './theme-slice';
+import {configureStore} from '@reduxjs/toolkit';
+import {languageSlice} from './language-slice';
+import {loginSlice} from './login-slice';
+import {themeSlice} from './theme-slice';
+
+import logger from 'redux-logger';
 
 export const appStore = configureStore({
-  reducer: {
-    language: languageSlice.reducer,
-    theme: themeSlice.reducer,
-    login: loginSlice.reducer,
-  },
+    reducer: {
+        language: languageSlice.reducer,
+        theme: themeSlice.reducer,
+        login: loginSlice.reducer,
+    },
+    middleware: [logger]
 });
