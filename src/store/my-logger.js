a763fd38a6e0e03e15c3383d81d45ae7691df3a3
Vivekanand P V <vivekanandpv@gmail.com>
2023-11-23 13:28:22 +0530

    done

--- src/store/my-logger.js
+++ src/store/my-logger.js
@@ -1,5 +1,6 @@
 export const myLogger = store => next => action => {
-    console.log('store', store.getState());
+    console.log('previous state', store.getState());
     console.log('action', action);
     next(action);
+    console.log('next state', store.getState());
 }
